e4e1911cb9232415be991f0f4bfe1b43460d2d6f
keyi-front <keyi-front@qq.com>
2025-12-06 18:01:09 +0800

    feat: scroll trigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import './App.css';
 
@@ -17,11 +17,47 @@ function App() {
     scrollListRef.current.scrollTop = dom.offsetTop + 20;
   }
 
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        console.log(entry.intersectionRatio);
+        console.log(entry.target.innerText);
+        // if (entry.intersectionRatio === 1) {
+        //   console.log(entry.target.innerText);
+        // }
+        // Each entry describes an intersection change for one observed
+        // target element:
+        //   entry.boundingClientRect
+        //   entry.intersectionRatio
+        //   entry.intersectionRect
+        //   entry.isIntersecting
+        //   entry.rootBounds
+        //   entry.target
+        //   entry.time
+      });
+    },
+    {
+      root: document.getElementById('content-list'),
+      rootMargin: '0px',
+      scrollMargin: '0px',
+      threshold: [0.25, 0.5, 1],
+    }
+  );
+  document.querySelectorAll('.list-item').forEach((item) => {
+    if (item) {
+      observer.observe(item);
+    }
+  });
+
   return (
     <>
-      <div className="content-list" ref={scrollListRef}>
+      <div className="content-list" id="content-list" ref={scrollListRef}>
         {upperCases.map((upperCase, index) => (
-          <div id={'block-' + upperCase} key={'block-' + index}>
+          <div
+            id={'block-' + upperCase}
+            className="list-item"
+            key={'block-' + index}
+          >
             <div className="block_header">{upperCase}</div>
             <div className="block_content"></div>
           </div>
